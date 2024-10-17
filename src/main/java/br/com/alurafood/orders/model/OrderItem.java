c0567d345b240656b3c375b9b81e46dd73333370
GustavoRidgue <gustavo.ridgue@gmail.com>
2024-10-16 22:56:55 -0300

    Start project with CRUD and register Eureka server

---
+++ src/main/java/br/com/alurafood/orders/model/OrderItem.java
@@ -0,0 +1,33 @@
+package br.com.alurafood.orders.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Entity
+@Table(name = "item_do_pedido")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Positive
+    private Integer quantidade;
+
+    private String descricao;
+
+    @ManyToOne(optional=false)
+    private Order order;
+
+}
---
+++ src/main/java/br/com/alurafood/orders/service/OrderService.java
@@ -0,0 +1,77 @@
+package br.com.alurafood.orders.service;
+
+import br.com.alurafood.orders.dto.OrderDTO;
+import br.com.alurafood.orders.dto.StatusDTO;
+import br.com.alurafood.orders.model.Order;
+import br.com.alurafood.orders.model.Status;
+import br.com.alurafood.orders.repository.OrderRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Autowired
+    private final ModelMapper modelMapper;
+
+
+    public List<OrderDTO> findAll() {
+        return repository.findAll().stream()
+                .map(p -> modelMapper.map(p, OrderDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public OrderDTO findById(Long id) {
+        Order order = repository.findById(id)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return modelMapper.map(order, OrderDTO.class);
+    }
+
+    public OrderDTO createOrder(OrderDTO dto) {
+        Order order = modelMapper.map(dto, Order.class);
+
+        order.setDataHora(LocalDateTime.now());
+        order.setStatus(Status.REALIZADO);
+        order.getItens().forEach(item -> item.setOrder(order));
+        Order salvo = repository.save(order);
+
+        return modelMapper.map(order, OrderDTO.class);
+    }
+
+    public OrderDTO updateStatus(Long id, StatusDTO dto) {
+
+        Order order = repository.putIdWithItems(id);
+
+        if (order == null) {
+            throw new EntityNotFoundException();
+        }
+
+        order.setStatus(dto.getStatus());
+        repository.updateStatus(dto.getStatus(), order);
+        return modelMapper.map(order, OrderDTO.class);
+    }
+
+    public void approveOrderPayment(Long id) {
+
+        Order order = repository.putIdWithItems(id);
+
+        if (order == null) {
+            throw new EntityNotFoundException();
+        }
+
+        order.setStatus(Status.PAGO);
+        repository.updateStatus(Status.PAGO, order);
+    }
+}
