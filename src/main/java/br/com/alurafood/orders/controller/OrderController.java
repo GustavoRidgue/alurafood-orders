ccb43445b1cae3de77b1edd08388376c04f46456
GustavoRidgue <gustavo.ridgue@gmail.com>
2024-10-18 20:12:05 -0300

    Add eureka instance and mvn wrapper

--- src/main/java/br/com/alurafood/orders/controller/OrderController.java
+++ src/main/java/br/com/alurafood/orders/controller/OrderController.java
@@ -17,44 +17,38 @@ import java.util.List;
 @RequestMapping("/orders")
 public class OrderController {
 
-        @Autowired
-        private OrderService service;
+    @Autowired
+    private OrderService service;
 
-        @GetMapping()
-        public List<OrderDTO> findAll() {
-            return service.findAll();
-        }
+    @GetMapping()
+    public List<OrderDTO> findAll() {
+        return service.findAll();
+    }
 
-        @GetMapping("/{id}")
-        public ResponseEntity<OrderDTO> findById(@PathVariable @NotNull Long id) {
-            OrderDTO dto = service.findById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDTO> findById(@PathVariable @NotNull Long id) {
+        OrderDTO dto = service.findById(id);
+        return  ResponseEntity.ok(dto);
+    }
 
-            return  ResponseEntity.ok(dto);
-        }
+    @PostMapping()
+    public ResponseEntity<OrderDTO> orderRequest(@RequestBody @Valid OrderDTO dto, UriComponentsBuilder uriBuilder) {
+        OrderDTO orderPlaced = service.createOrder(dto);
 
-        @PostMapping()
-        public ResponseEntity<OrderDTO> orderRequest(@RequestBody @Valid OrderDTO dto, UriComponentsBuilder uriBuilder) {
-            OrderDTO orderPlaced = service.createOrder(dto);
+        URI address = uriBuilder.path("/orders/{id}").buildAndExpand(orderPlaced.getId()).toUri();
+        return ResponseEntity.created(address).body(orderPlaced);
+    }
 
-            URI address = uriBuilder.path("/orders/{id}").buildAndExpand(orderPlaced.getId()).toUri();
-
-            return ResponseEntity.created(address).body(orderPlaced);
-
-        }
-
-        @PutMapping("/{id}/status")
-        public ResponseEntity<OrderDTO> updateStatus(@PathVariable Long id, @RequestBody StatusDTO status){
-           OrderDTO dto = service.updateStatus(id, status);
-
-            return ResponseEntity.ok(dto);
-        }
+    @PutMapping("/{id}/status")
+    public ResponseEntity<OrderDTO> updateStatus(@PathVariable Long id, @RequestBody StatusDTO status){
+        OrderDTO dto = service.updateStatus(id, status);
+        return ResponseEntity.ok(dto);
+    }
 
 
-        @PutMapping("/{id}/paid")
-        public ResponseEntity<Void> approvePayment(@PathVariable @NotNull Long id) {
-            service.approveOrderPayment(id);
-
-            return ResponseEntity.ok().build();
-
-        }
+    @PutMapping("/{id}/paid")
+    public ResponseEntity<Void> approvePayment(@PathVariable @NotNull Long id) {
+        service.approveOrderPayment(id);
+        return ResponseEntity.ok().build();
+    }
 }
